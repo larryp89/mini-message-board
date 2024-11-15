dc732eff55fe0abe88dd99e00c6f8f92c9df75f5
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-11-15 08:10:29 +0000

    Add route to open individual message

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,17 @@
+const crypto = require("crypto");
+
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
+    id: crypto.randomUUID(),
   },
   {
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
+    id: crypto.randomUUID(),
   },
 ];
 
@@ -17,14 +21,28 @@ const { Router } = require("express");
 // Create an instance of the Express router
 const indexRouter = Router();
 
+// Create main GET index route
 indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+// Route to get message by id
+indexRouter.get("/messages/:id", (req, res) => {
+  const messageID = req.params.id; // Get the message id from the URL
+  const message = messages.find((m) => messageID === m.id);
+  res.render("messageDetail", { message });
+});
+
+// Create POST route
 indexRouter.post("/new", (req, res) => {
   // Add the new message to the messages array
   const { messageText, username } = req.body;
-  messages.push({ text: messageText, user: username, added: new Date() });
+  messages.push({
+    text: messageText,
+    user: username,
+    added: new Date(),
+    id: crypto.randomUUID(),
+  });
 
   // Redirect back to the home page to see the updated messages list
   res.redirect("/");
